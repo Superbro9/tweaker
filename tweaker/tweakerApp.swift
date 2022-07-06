ee8a5d7bd09a78b25c1055238fb84fe32a36e5e9
Superbro <65824876+Superbro9@users.noreply.github.com>
2022-07-06 16:15:14 +0100

    Initial Commit

---
+++ tweaker/tweakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  tweakerApp.swift
+//  tweaker
+//
+//  Created by Hugo Mason on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct tweakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
